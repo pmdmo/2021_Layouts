b7252ae2ed3a247ca7846754fbe86487782a000a
pedrojoya <pedrojoya@gmail.com>
2021-10-29 13:00:35 +0200

    Initial commit

---
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -0,0 +1,12 @@
+package es.iessaladillo.pedrojoya.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linearlayout_activity)
+    }
+}
